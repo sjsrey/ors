43a107c327ede2d2f52712bc05a32f2a44e321a6
Serge Rey <sjsrey@gmail.com>
2013-06-30 20:26:55 -0700

    - mindmap

---
+++ ors.mm
@@ -0,0 +1,166 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ors" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1372343253508"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10"/>
+<node TEXT="purpose of science" POSITION="right" ID="ID_1359902778" CREATED="1372343254076" MODIFIED="1372343259182">
+<edge COLOR="#ff0000"/>
+<node TEXT="production of new knowledge" ID="ID_499091118" CREATED="1372344157133" MODIFIED="1372344163464"/>
+<node TEXT="betterment of human kind" ID="ID_326441982" CREATED="1372344148782" MODIFIED="1372344156352"/>
+</node>
+<node TEXT="self correction" POSITION="right" ID="ID_1615675816" CREATED="1372343260852" MODIFIED="1372343266431">
+<edge COLOR="#0000ff"/>
+<node TEXT="what it is" ID="ID_1992309618" CREATED="1372343266899" MODIFIED="1372343268863"/>
+<node TEXT="relies on openness" ID="ID_1542567509" CREATED="1372343269251" MODIFIED="1372343272990"/>
+<node TEXT="open science is under attack" ID="ID_1901181050" CREATED="1372343273409" MODIFIED="1372343279447"/>
+<node TEXT="loss of self correcting mechnaims" ID="ID_1427811630" CREATED="1372343279738" MODIFIED="1372343285894"/>
+</node>
+<node TEXT="commercialization of science" POSITION="right" ID="ID_178731356" CREATED="1372343288537" MODIFIED="1372343294086">
+<edge COLOR="#00ff00"/>
+<node TEXT="publishing" ID="ID_706562588" CREATED="1372343294449" MODIFIED="1372343296470">
+<node TEXT="cost of publishing" ID="ID_1150591647" CREATED="1372343296929" MODIFIED="1372343302805"/>
+<node TEXT="cost of reading" ID="ID_196056471" CREATED="1372343303170" MODIFIED="1372343306461"/>
+<node TEXT="time to publish" ID="ID_1350483733" CREATED="1372343306824" MODIFIED="1372343312308"/>
+</node>
+<node TEXT="contrast with open publishing" ID="ID_632402608" CREATED="1372343314032" MODIFIED="1372343321452">
+<node TEXT="shorter pub times" ID="ID_1773114076" CREATED="1372343327411" MODIFIED="1372343333159"/>
+</node>
+</node>
+<node TEXT="journals" POSITION="right" ID="ID_474476836" CREATED="1372343381297" MODIFIED="1372343384077">
+<edge COLOR="#00ffff"/>
+<node TEXT="limited support for reproducability" ID="ID_1050989583" CREATED="1372343384377" MODIFIED="1372343392732">
+<node TEXT="papers exclucde" ID="ID_343924927" CREATED="1372343555428" MODIFIED="1372343559694">
+<node TEXT="software" ID="ID_1604109900" CREATED="1372343560035" MODIFIED="1372343561646"/>
+<node TEXT="data" ID="ID_601605865" CREATED="1372343561963" MODIFIED="1372343562854"/>
+<node TEXT="increasingly complex analyses" ID="ID_951136041" CREATED="1372343563283" MODIFIED="1372343570142">
+<node TEXT="scale" ID="ID_1687535640" CREATED="1372343570458" MODIFIED="1372343571774"/>
+<node TEXT="doen&apos;t fit in the old model" ID="ID_1481076870" CREATED="1372343573155" MODIFIED="1372343577430"/>
+</node>
+</node>
+<node TEXT="check out regional science journals" ID="ID_1144799588" CREATED="1372343618989" MODIFIED="1372343625719">
+<node TEXT="how many require data and software for accepted articles" ID="ID_1608984232" CREATED="1372343626197" MODIFIED="1372343639158"/>
+</node>
+</node>
+<node TEXT="if you cant reproduce it, it doesn&apos;t exist" ID="ID_1193482447" CREATED="1372343393112" MODIFIED="1372343407900"/>
+</node>
+<node TEXT="review process" POSITION="right" ID="ID_995116997" CREATED="1372343336243" MODIFIED="1372343342279">
+<edge COLOR="#ff00ff"/>
+<node TEXT="receives less attention in the open publishing debate" ID="ID_240827055" CREATED="1372343342699" MODIFIED="1372343355126"/>
+<node TEXT="candor through confidentialitty generally viewed as a good thing" ID="ID_1086150936" CREATED="1372343355499" MODIFIED="1372343373614"/>
+<node TEXT="but is it always good?" ID="ID_679933209" CREATED="1372343373945" MODIFIED="1372345427386"/>
+</node>
+<node TEXT="open science" POSITION="right" ID="ID_1805464243" CREATED="1372343813273" MODIFIED="1372343835708">
+<edge COLOR="#7c0000"/>
+<node TEXT="open access" ID="ID_1359420150" CREATED="1372343822352" MODIFIED="1372343824924">
+<node TEXT="" ID="ID_1362784540" CREATED="1372344700718" MODIFIED="1372344700718"/>
+</node>
+<node TEXT="open source" ID="ID_1041666499" CREATED="1372343825369" MODIFIED="1372343827972"/>
+<node TEXT="open data" ID="ID_55739831" CREATED="1372343828481" MODIFIED="1372343831492"/>
+</node>
+<node TEXT="data hoarding" POSITION="right" ID="ID_1963479618" CREATED="1372344012787" MODIFIED="1372344015743">
+<edge COLOR="#00007c"/>
+<node TEXT="proprietary data" ID="ID_2178514" CREATED="1372344321320" MODIFIED="1372344331978">
+<node TEXT="data analytics" ID="ID_271919947" CREATED="1372344369989" MODIFIED="1372344373337"/>
+<node TEXT="census research centers" ID="ID_1480161861" CREATED="1372344373781" MODIFIED="1372344378672"/>
+</node>
+<node TEXT="first users" ID="ID_1028446216" CREATED="1372344332519" MODIFIED="1372344338626"/>
+</node>
+<node TEXT="correlation between sharing and citation" POSITION="right" ID="ID_669076238" CREATED="1372344016524" MODIFIED="1372344027111">
+<edge COLOR="#007c00"/>
+<node TEXT="nature paper" ID="ID_123275332" CREATED="1372344027412" MODIFIED="1372344029391"/>
+</node>
+<node TEXT="alternatives" POSITION="right" ID="ID_27099479" CREATED="1372343506013" MODIFIED="1372343509174">
+<edge COLOR="#ffff00"/>
+<node TEXT="github" ID="ID_1785581541" CREATED="1372343509555" MODIFIED="1372343510950">
+<node TEXT="everything is open" ID="ID_1970619704" CREATED="1372343511283" MODIFIED="1372343517110"/>
+<node TEXT="everything is replicable" ID="ID_1872759942" CREATED="1372343517506" MODIFIED="1372343521166"/>
+<node TEXT="in the open source world, if it isn&apos;t up there it doesn&apos;t count" ID="ID_1449244558" CREATED="1372343521626" MODIFIED="1372343532621"/>
+</node>
+<node TEXT="commercial publishers" ID="ID_1870211970" CREATED="1372343581370" MODIFIED="1372343584605">
+<node TEXT="new models" ID="ID_402348172" CREATED="1372343584978" MODIFIED="1372343587421">
+<node TEXT="open access isn&apos;t it" ID="ID_1985999441" CREATED="1372343587929" MODIFIED="1372343592013">
+<node TEXT="replication not addresses" ID="ID_1830355392" CREATED="1372343592401" MODIFIED="1372343597653"/>
+</node>
+<node TEXT="optical society of america" ID="ID_480114968" CREATED="1372343605833" MODIFIED="1372343661779">
+<node TEXT="pdfs w embedded linnks" ID="ID_695375374" CREATED="1372343662192" MODIFIED="1372343667139">
+<node TEXT="pulls downd data" ID="ID_194382872" CREATED="1372343672231" MODIFIED="1372343679091"/>
+<node TEXT="pulls down methods" ID="ID_642547406" CREATED="1372343679359" MODIFIED="1372343682235"/>
+<node TEXT="reproduce what paper is showing you in a direct way" ID="ID_734992776" CREATED="1372343682800" MODIFIED="1372343692682"/>
+</node>
+</node>
+<node TEXT="science direct" ID="ID_1209646054" CREATED="1372343695822" MODIFIED="1372343699850">
+<node TEXT="elsevier" ID="ID_711797854" CREATED="1372343700590" MODIFIED="1372343702674"/>
+</node>
+<node TEXT="these don&apos;t yet deal with software" ID="ID_1255310598" CREATED="1372343720896" MODIFIED="1372343732332"/>
+<node TEXT="see the inisght journal" ID="ID_443929638" CREATED="1372343743454" MODIFIED="1372343748953">
+<node TEXT="kitware product" ID="ID_249362613" CREATED="1372344956388" MODIFIED="1372344960182">
+<node TEXT="ask matt hoffman" ID="ID_685798175" CREATED="1372344960579" MODIFIED="1372344965078"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="open science framework" ID="ID_1253224334" CREATED="1372360910467" MODIFIED="1372360914950">
+<node TEXT="guys from scipy meeting" ID="ID_137354796" CREATED="1372360915507" MODIFIED="1372360922454"/>
+</node>
+</node>
+<node TEXT="to do" POSITION="left" ID="ID_1952764226" CREATED="1372344470767" MODIFIED="1372344472346">
+<edge COLOR="#7c007c"/>
+<node TEXT="get costs on publishing" ID="ID_186152510" CREATED="1372344472751" MODIFIED="1372344476098"/>
+<node TEXT="get his slides" ID="ID_352142260" CREATED="1372344476534" MODIFIED="1372344480058"/>
+<node TEXT="what is scientific publishing" ID="ID_1292445334" CREATED="1372344523685" MODIFIED="1372344528632">
+<node TEXT="publish" ID="ID_500615343" CREATED="1372344529093" MODIFIED="1372344530576">
+<node TEXT="to make publicly known" ID="ID_76474856" CREATED="1372344761189" MODIFIED="1372344766880"/>
+</node>
+<node TEXT="science" ID="ID_3159556" CREATED="1372344530996" MODIFIED="1372344532544"/>
+</node>
+<node TEXT="research artifacts" ID="ID_528521629" CREATED="1372344712971" MODIFIED="1372344716102"/>
+</node>
+</node>
+</map>
