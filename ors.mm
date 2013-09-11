6e62965944554c86bf827821c4a77c049e1ba38b
Serge Rey <sjsrey@gmail.com>
2013-09-11 06:41:12 -0700

    - pulled in todo and notes from omnifocus

--- ors.mm
+++ ors.mm
@@ -49,7 +49,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="10"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
 <node TEXT="purpose of science" POSITION="right" ID="ID_1359902778" CREATED="1372343254076" MODIFIED="1372343259182">
 <edge COLOR="#ff0000"/>
 <node TEXT="production of new knowledge" ID="ID_499091118" CREATED="1372344157133" MODIFIED="1372344163464"/>
@@ -102,7 +102,10 @@
 <node TEXT="" ID="ID_1362784540" CREATED="1372344700718" MODIFIED="1372344700718"/>
 </node>
 <node TEXT="open source" ID="ID_1041666499" CREATED="1372343825369" MODIFIED="1372343827972"/>
-<node TEXT="open data" ID="ID_55739831" CREATED="1372343828481" MODIFIED="1372343831492"/>
+<node TEXT="open data" ID="ID_55739831" CREATED="1372343828481" MODIFIED="1372343831492">
+<node TEXT="sharing data serf google" ID="ID_1788697555" CREATED="1378906729243" MODIFIED="1378906742629" LINK="http://www.nytimes.com/2013/08/13/science/how-to-share-scientific-data.html?_r=0"/>
+<node TEXT="geodata" ID="ID_1312639857" CREATED="1378906832340" MODIFIED="1378906835685" LINK="http://blog.geomusings.com/2013/07/28/devops-for-geospatial-data/"/>
+</node>
 </node>
 <node TEXT="data hoarding" POSITION="right" ID="ID_1963479618" CREATED="1372344012787" MODIFIED="1372344015743">
 <edge COLOR="#00007c"/>
@@ -150,6 +153,28 @@
 <node TEXT="guys from scipy meeting" ID="ID_137354796" CREATED="1372360915507" MODIFIED="1372360922454"/>
 </node>
 </node>
+<node TEXT="ideas from drobbox" POSITION="right" ID="ID_434236752" CREATED="1378905239712" MODIFIED="1378905248597">
+<edge COLOR="#007c7c"/>
+<node TEXT="SciPy conf open referee process&#xa;2013-08-05&#xa;Editor as bdfl&#xa;&#xa;&#xa;Cost of open access on NSF grant&#xa;&#xa;Open data not enough&#xa;Open code not enough&#xa;&#xa;Platform needs to also be open&#xa;&#xa;Translucency versus transparency&#xa;2013-08-05&#xa;This is a test&#xa;Editor as bdfl&#xa;&#xa;&#xa;Cost of open access on NSF grant&#xa;&#xa;Open data not enough&#xa;Open code not enough&#xa;&#xa;Platform needs to also be open&#xa;&#xa;Translucency versus transparency&#xa;Cost of openaccess&#xa;&#xa;6k per article&#xa;Express relative to average NSF grant&#xa;Take account of overhead&#xa;Data alone is not enough for replication. You also need the code.&#xa;&#xa;Code and data is not enough for replication, you also need a fully open platform.&#xa;&#xa;&quot;Here is my data and stata-sas-spss script&quot; isn&apos;t going to support full replication.&#xa;&#xa;Rounding issues cross platform. Not an easy problem but one more transparent on open platforms&#xa;&#xa;&#xa;&#xa;Translucency versus transparency. The former is applicable in the closed platform open code and data case, while the latter is open platform, code and data.&#xa;&#xa;Goal is science. That should drive our decisions as scientists.&#xa;Editor as pull request manager, refs and authors as submitters&#xa;&#xa;&#xa;Proposal review anonymous on review side. Candor versus collaboration. Would this be different if the reviews were not anonymous.&#xa;&#xa;Small resources biases towards previous record. Novel ideas get funded less often." ID="ID_1122025299" CREATED="1378905250353" MODIFIED="1378905252694"/>
+</node>
+<node TEXT="replication" POSITION="right" ID="ID_1765295010" CREATED="1378905780413" MODIFIED="1378905783217">
+<edge COLOR="#7c7c00"/>
+<node TEXT="costly to do" ID="ID_1451491773" CREATED="1378905783708" MODIFIED="1378905786905">
+<node TEXT="after the fact" ID="ID_1389799049" CREATED="1378905787317" MODIFIED="1378905789785"/>
+<node TEXT="we should start a project with replication mechanisms in place" ID="ID_969226005" CREATED="1378905790166" MODIFIED="1378905807971">
+<node TEXT="not after we are done" ID="ID_1357480089" CREATED="1378905808414" MODIFIED="1378905812564"/>
+<node TEXT="test driven development analogy" ID="ID_97358193" CREATED="1378905813368" MODIFIED="1378905828675" LINK="A%20Development%20Methodology%20for%20Scientific%20Software">
+<node TEXT="\cite{Cort:1985fk}" ID="ID_1363384655" CREATED="1378905853116" MODIFIED="1378905853116"/>
+</node>
+</node>
+<node TEXT="ideas on referee process akin to software development" ID="ID_883283520" CREATED="1378905867278" MODIFIED="1378905883450">
+<node TEXT="---------- Forwarded message ----------&#xa;From:&#xa;Date: Tuesday, June 4, 2013&#xa;Subject: [SciPy-User] peer review of scientific software&#xa;To: SciPy Users List &lt;scipy-user@scipy.org&gt;&#xa;On Tue, Jun 4, 2013 at 7:27 AM,  &lt;josef.pktd@gmail.com&gt; wrote:&#xa; &gt; On Tue, Jun 4, 2013 at 4:07 AM, Suzen, Mehmet &lt;msuzen@gmail.com&gt; wrote:&#xa; &gt;&gt; On 28 May 2013 20:23, Calvin Morrison &lt;mutantturkey@gmail.com&gt; wrote:&#xa; &gt;&gt;&gt;&#xa; &gt;&gt;&gt; http://arxiv.org/pdf/1210.0530v3.pdf&#xa; &gt;&gt;&gt;&#xa; &gt;&gt;&gt; Pissed-off Scientific Programmer,&#xa; &gt;&gt;&gt; Calvin Morrison&#xa; &gt;&gt;&#xa; &gt;&gt; Those recent papers and discussions all talk about good practises. I&#xa; &gt;&gt; was thinking&#xa; &gt;&gt; today in the bus, why there are not many literature on scientific&#xa; &gt;&gt; software development&#xa; &gt;&gt; methodologies. One explicit paper I found was from 80s called&#xa; &gt;&gt;&#xa; &gt;&gt; A Development Methodology for Scientific Software&#xa; &gt;&gt; Cort, G. et. al.&#xa; &gt;&gt; http://dx.doi.org/10.1109/TNS.1985.4333629&#xa; &gt;&gt;&#xa; &gt;&gt; It is pretty classic approach for today&apos;s standard,  There is also a book about&#xa; &gt;&gt; generic style and good practice, its a pretty good book (might be&#xa; &gt;&gt; mentioned in this list before):&#xa; &gt;&gt;&#xa; &gt;&gt; Writing Scientific Software: A Guide to Good Style&#xa; &gt;&gt; Suely Oliveira and David E. Stewart&#xa; &gt;&gt; http://www.cambridge.org/9780521858960&#xa; &gt;&gt;&#xa; &gt;&gt; but I don&apos;t see any reference to modern development methodologies specifically&#xa; &gt;&gt; address to scientific software. For example: extensions of test driven&#xa; &gt;&gt; development,&#xa; &gt;&gt; which would suit better than classic&#xa; &gt;&gt; specification-design-coding-testing. Test cases&#xa; &gt;&gt; would be directly related to what we would like to achieve in the&#xa; &gt;&gt; first place. For example&#xa; &gt;&gt; a generic density of something etc. I haven&apos;t heard anyone developing&#xa; &gt;&gt; scientific software&#xa; &gt;&gt; in this way...yet.&#xa; &gt;&#xa; &gt; I think functional (not unit) testing is pretty much the standard in&#xa; &gt; the area of developing statistical algorithms even if nobody calls it&#xa; &gt; that way. And I don&apos;t know of any references to software development&#xa; &gt; for it.&#xa; &gt;&#xa; &gt; When writing a library function for existing algorithms, then it is&#xa; &gt; standard to test it against existing results. Many (or most) software&#xa; &gt; packages, or articles that describe the software, show that they&#xa; &gt; reproduce existing results as test cases.&#xa; &gt; (And that&apos;s the way we work for statsmodels.)&#xa; &gt;&#xa; &gt; For new algorithms, it is standard to publish Monte Carlo studies that&#xa; &gt; show that the new algorithm is &quot;better&quot; in at least some cases or&#xa; &gt; directions than the existing algorithms (or statistical estimators and&#xa; &gt; tests), and often they use published case studies or applied results&#xa; &gt; to show how the conclusion would differ or be unchanged&#xa; &gt;&#xa; &gt; (Just for illustration: the workflow of some friends of mine that are&#xa; &gt; theoretical econometricians.&#xa; &gt; First write the paper with the heavy theory and proofs, then start to&#xa; &gt; write the MonteCarlo, the first version doesn&apos;t deliver the results&#xa; &gt; that can be expected based on the theory, look for bugs and fix those,&#xa; &gt; rerun MonteCarlo, iterate, then find different test cases, simulated&#xa; &gt; data generating processes, and show where it works and where it&#xa; &gt; doesn&apos;t, and check the theoretical explanation/intuition why it&#xa; &gt; doesn&apos;t work in some cases. Submit only cases that work, and write a&#xa; &gt; footnote for the other cases.&#xa; Sorry I forgot one step&#xa; After the submission, one referee of the paper doesn&apos;t like some parts&#xa; or wants additional simulations. Iterate until publication or&#xa; rejection. If rejection, then submit to another journal, and iterate.&#xa; By the time the article is finally published, other researchers&#xa; already started to use the algorithm and possibly the code.&#xa; Sounds partially like functional test driven developement to me.&#xa; )&#xa; &gt;&#xa; &gt; And after, that there are many published articles that present&#xa; &gt; MonteCarlo studies to show that an algorithm does not work properly if&#xa; &gt; some assumptions are violated, and that something else is better.&#xa; &gt;&#xa; &gt; (This doesn&apos;t mean that they produce a &quot;pretty&quot; piece of software, but&#xa; &gt; it shows that it works as advertised.)&#xa; &gt;&#xa; &gt;&#xa; &gt; I don&apos;t think I ever heard of unit or functional testing for applied&#xa; &gt; research, that is testing the workflow and not the computational&#xa; &gt; tools.&#xa; &gt;&#xa; &gt; Josef&#xa; &gt;&#xa; &gt;&gt;&#xa; &gt;&gt; Best,&#xa; &gt;&gt; -m&#xa; &gt;&gt; _______________________________________________&#xa; &gt;&gt; SciPy-User mailing list&#xa; &gt;&gt; SciPy-User@scipy.org&#xa; &gt;&gt; http://mail.scipy.org/mailman/listinfo/scipy-user&#xa; _______________________________________________&#xa; SciPy-User mailing list&#xa; SciPy-User@scipy.org&#xa; http://mail.scipy.org/mailman/listinfo/scipy-user&#xa;--&#xa;Sergio (Serge) Rey&#xa;Professor, School of Geographical Sciences and Urban Planning&#xa;GeoDa Center for Geospatial Analysis and Computation&#xa;Arizona State University&#xa;http://geoplan.asu.edu/rey&#xa;Editor, International Regional Science Review&#xa;http://irx.sagepub.com" ID="ID_202813144" CREATED="1378905960094" MODIFIED="1378905963900"/>
+</node>
+<node TEXT="what scientific programming can learn from software practices" ID="ID_1146331973" CREATED="1378906072234" MODIFIED="1378906091359"/>
+<node TEXT="and what software practices can learn from science" ID="ID_330392685" CREATED="1378906091938" MODIFIED="1378906102264"/>
+</node>
+<node TEXT="quotes" ID="ID_460327894" CREATED="1378906659627" MODIFIED="1378906663777" LINK="https://www.wakari.io/nb/url///wakari.io/static/notebooks/Lecture_0_Scientific_Computing_with_Python.ipynb"/>
+</node>
 <node TEXT="to do" POSITION="left" ID="ID_1952764226" CREATED="1372344470767" MODIFIED="1372344472346">
 <edge COLOR="#7c007c"/>
 <node TEXT="get costs on publishing" ID="ID_186152510" CREATED="1372344472751" MODIFIED="1372344476098"/>
@@ -161,6 +186,46 @@
 <node TEXT="science" ID="ID_3159556" CREATED="1372344530996" MODIFIED="1372344532544"/>
 </node>
 <node TEXT="research artifacts" ID="ID_528521629" CREATED="1372344712971" MODIFIED="1372344716102"/>
+</node>
+<node TEXT="outline" POSITION="left" ID="ID_248530387" CREATED="1378906174267" MODIFIED="1378906175887">
+<edge COLOR="#ff0000"/>
+<node TEXT="introduction" ID="ID_1619288294" CREATED="1378906181172" MODIFIED="1378906183624">
+<node TEXT="define regional science" ID="ID_1956622211" CREATED="1378906425828" MODIFIED="1378906428784">
+<node TEXT="neil" ID="ID_1242915499" CREATED="1378906472208" MODIFIED="1378906475453" LINK="http://www.research-europe.com/index.php/2013/08/dr-neil-reid-executive-director-north-american-regional-science-council/"/>
+</node>
+<node TEXT="define science" ID="ID_93584344" CREATED="1378906430612" MODIFIED="1378906435176"/>
+<node TEXT="define open" ID="ID_1935367217" CREATED="1378906435653" MODIFIED="1378906439153"/>
+<node TEXT="outline of paper/arguments" ID="ID_1000330695" CREATED="1378906439709" MODIFIED="1378906446457"/>
+</node>
+<node TEXT="problem" ID="ID_898945172" CREATED="1378906184124" MODIFIED="1378906193144">
+<node TEXT="science" ID="ID_1139227976" CREATED="1378906210111" MODIFIED="1378906215453"/>
+<node TEXT="commercialization of science" ID="ID_686459152" CREATED="1378906215807" MODIFIED="1378906220075">
+<node TEXT="publication model" ID="ID_94336192" CREATED="1378906491586" MODIFIED="1378906494054">
+<node TEXT="gelman" ID="ID_43545418" CREATED="1378906517901" MODIFIED="1378906521057" LINK="http://andrewgelman.com/2013/07/02/19268/"/>
+</node>
+<node TEXT="open access" ID="ID_672726568" CREATED="1378906578683" MODIFIED="1378906582831">
+<node TEXT="sage" ID="ID_689768824" CREATED="1378906583764" MODIFIED="1378906586473" LINK="http://sgo.sagepub.com/content/current"/>
+</node>
+</node>
+<node TEXT="replication problems" ID="ID_470752883" CREATED="1378906220791" MODIFIED="1378906227220"/>
+<node TEXT="data hoarding" ID="ID_1394648343" CREATED="1378906251906" MODIFIED="1378906256143"/>
+<node TEXT="methods hoarding" ID="ID_820699142" CREATED="1378906256636" MODIFIED="1378906260255"/>
+</node>
+<node TEXT="solutions" ID="ID_495377588" CREATED="1378906229305" MODIFIED="1378906231204">
+<node TEXT="open science" ID="ID_919739252" CREATED="1378906246698" MODIFIED="1378906249014"/>
+<node TEXT="correlation between sharing and citation" ID="ID_1170014806" CREATED="1378906262476" MODIFIED="1378906268136"/>
+<node TEXT="review process" ID="ID_957088108" CREATED="1378906270836" MODIFIED="1378906273688"/>
+<node TEXT="journals" ID="ID_1595190472" CREATED="1378906275165" MODIFIED="1378906279193">
+<node TEXT="new models" ID="ID_1690558888" CREATED="1378906283757" MODIFIED="1378906285393">
+<node TEXT="front the costs of replication infrastructure" ID="ID_1558239315" CREATED="1378906285790" MODIFIED="1378906297187">
+<node TEXT="scales better with a publisher than individual scientist" ID="ID_1992075622" CREATED="1378906297607" MODIFIED="1378906309717"/>
+</node>
+</node>
+</node>
+<node TEXT="learn from our mistakes" ID="ID_1456326917" CREATED="1378906381295" MODIFIED="1378906384900">
+<node TEXT="Dennet book and the importance of making mistakes as a way to learn. Link this to openness and publication bias" ID="ID_1269257663" CREATED="1378906394025" MODIFIED="1378906395453"/>
+</node>
+</node>
 </node>
 </node>
 </map>
