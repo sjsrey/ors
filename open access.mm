4a932aa7a766e39ef00df05063965b1df19121a4
Serge Rey <sjsrey@gmail.com>
2013-10-16 11:11:23 -0700

    open access draft

---
+++ open access.mm
@@ -0,0 +1,134 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="open access" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1381943704005"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="source" POSITION="right" ID="ID_1916472961" CREATED="1381943921862" MODIFIED="1381943923282">
+<edge COLOR="#ff00ff"/>
+<node TEXT="http://opensource.com/life/13/10/tipping-point-open-access-science" ID="ID_241928611" CREATED="1381943925767" MODIFIED="1381943933117" LINK="http://opensource.com/life/13/10/tipping-point-open-access-science">
+<node TEXT="By 2011 50% of all scientific articles published in 2011 are in some form of open acess" ID="ID_700178631" CREATED="1381943705130" MODIFIED="1381943938272" LINK="http://opensource.com/life/13/10/tipping-point-open-access-science"/>
+<node TEXT="subscription based journals on the way out" ID="ID_1827409171" CREATED="1381943756327" MODIFIED="1381943938292"/>
+<node TEXT="pressures behind open access" ID="ID_1556257889" CREATED="1381943777064" MODIFIED="1381943938306">
+<node TEXT="The Cost of knowledge" ID="ID_1160237404" CREATED="1381943783898" MODIFIED="1381943790374">
+<node TEXT="14000 scienties signed pledge" ID="ID_637886821" CREATED="1381943790746" MODIFIED="1381943801071">
+<node TEXT="publishing" ID="ID_949655119" CREATED="1381943801539" MODIFIED="1381943809776"/>
+<node TEXT="refereeing" ID="ID_1916861283" CREATED="1381943810180" MODIFIED="1381943812856"/>
+<node TEXT="being involved in the editorial process" ID="ID_1063740062" CREATED="1381943813131" MODIFIED="1381943825353"/>
+</node>
+<node TEXT="due to Elsevier charging  exorbitantly high prices for subscriptions to indivdual journals" ID="ID_80908411" CREATED="1381943827757" MODIFIED="1381943857461"/>
+<node TEXT="publisher adds relatively littel value to the publishing process" ID="ID_1329277077" CREATED="1381943859712" MODIFIED="1381943874294"/>
+</node>
+<node TEXT="Governemtn" ID="ID_1055911619" CREATED="1381943878027" MODIFIED="1381943880447">
+<node TEXT="publishing results of federally funded research" ID="ID_632240535" CREATED="1381943880859" MODIFIED="1381943889847"/>
+</node>
+<node TEXT="university" ID="ID_1606339330" CREATED="1381943942224" MODIFIED="1381943945084">
+<node TEXT="UC adopted open access policy for all publications authored by its researchers" ID="ID_1715837783" CREATED="1381943945553" MODIFIED="1381943963991"/>
+</node>
+</node>
+<node TEXT="point about future of all academia moving towards public&apos;s best interest and away from obsolete publishing companies to greater dissemniation of results" ID="ID_960848898" CREATED="1381943970060" MODIFIED="1381944010971">
+<node TEXT="Kahn" ID="ID_669863385" CREATED="1381944012071" MODIFIED="1381944018132"/>
+<node TEXT="OpenCourseWare MIT" ID="ID_81630077" CREATED="1381944018448" MODIFIED="1381944023500"/>
+<node TEXT="Moocs" ID="ID_550166390" CREATED="1381944023840" MODIFIED="1381944025492"/>
+</node>
+</node>
+<node TEXT="Whos afraid of Peer Review" ID="ID_1758295192" CREATED="1381944094928" MODIFIED="1381944217710" LINK="http://www.sciencemag.org/content/342/6154/60.full">
+<node TEXT="Spoof submissions getting published" ID="ID_32190826" CREATED="1381944122482" MODIFIED="1381944133095">
+<node TEXT="Journal of Natural Pharmaceuticals" ID="ID_1320704282" CREATED="1381944135323" MODIFIED="1381944145712">
+<node TEXT="Cobange from lichen having anti cancer properties" ID="ID_720141041" CREATED="1381944146092" MODIFIED="1381944160330"/>
+<node TEXT="Cobange doesn&apos;t exist" ID="ID_654673928" CREATED="1381944161101" MODIFIED="1381944166162"/>
+</node>
+<node TEXT="Submitted 304 versions of wonder drug papger to open-access journals" ID="ID_438624179" CREATED="1381944179536" MODIFIED="1381944194485">
+<node TEXT="more than half have been accepted" ID="ID_339837069" CREATED="1381944195482" MODIFIED="1381944202351"/>
+</node>
+<node TEXT="acceptance is the norm not the exception" ID="ID_1430199582" CREATED="1381944253192" MODIFIED="1381944262436">
+<node TEXT="Journals hosted by" ID="ID_1404160671" CREATED="1381944273361" MODIFIED="1381944277333">
+<node TEXT="Sage" ID="ID_790640229" CREATED="1381944277674" MODIFIED="1381944278925">
+<node TEXT="Journal of International Medical Research" ID="ID_1074652600" CREATED="1381944761241" MODIFIED="1381944772781"/>
+</node>
+<node TEXT="Elsevier" ID="ID_1829866139" CREATED="1381944279322" MODIFIED="1381944282390">
+<node TEXT="Drug Intervention Today" ID="ID_703863547" CREATED="1381944774948" MODIFIED="1381944780855"/>
+</node>
+</node>
+</node>
+<node TEXT="how it was done" ID="ID_1937224253" CREATED="1381944586928" MODIFIED="1381944590500">
+<node TEXT="random fictiinoal authors" ID="ID_500832786" CREATED="1381944590881" MODIFIED="1381944596564"/>
+<node TEXT="combinations of molecules, species and cancer cells" ID="ID_322152384" CREATED="1381944596968" MODIFIED="1381944609333"/>
+</node>
+</node>
+<node TEXT="Model" ID="ID_1093506190" CREATED="1381944358570" MODIFIED="1381944359773">
+<node TEXT="fees" ID="ID_333277071" CREATED="1381944641797" MODIFIED="1381944643089">
+<node TEXT="submission fees" ID="ID_1552033099" CREATED="1381944643437" MODIFIED="1381944646858"/>
+<node TEXT="publication fees" ID="ID_1732661788" CREATED="1381944647286" MODIFIED="1381944652042">
+<node TEXT="open access feels" ID="ID_1468596386" CREATED="1381944652326" MODIFIED="1381944657346"/>
+</node>
+<node TEXT="acceptance leads to publication feels" ID="ID_1528505282" CREATED="1381944360177" MODIFIED="1381944370030"/>
+</node>
+<node TEXT="open access journals &quot;parasitizing the scientific research community&quot;" ID="ID_1696692003" CREATED="1381944372891" MODIFIED="1381944386336"/>
+<node TEXT="get names on editorial board" ID="ID_223772206" CREATED="1381944429001" MODIFIED="1381944434876">
+<node TEXT="over-rule their recommendations" ID="ID_584060985" CREATED="1381944435337" MODIFIED="1381944444294"/>
+<node TEXT="leave them on the boards even after requesting remove" ID="ID_1846878544" CREATED="1381944444578" MODIFIED="1381944452766"/>
+</node>
+</node>
+<node TEXT="response" ID="ID_415639579" CREATED="1381944491414" MODIFIED="1381944492690">
+<node TEXT="UC Denber - Jeffrey Beall" ID="ID_1358433758" CREATED="1381944493062" MODIFIED="1381944505835">
+<node TEXT="listing and shaming predatory publishers" ID="ID_1914512323" CREATED="1381944506247" MODIFIED="1381944519621">
+<node TEXT="undisclosed charges" ID="ID_1885312705" CREATED="1381944520000" MODIFIED="1381944526349"/>
+<node TEXT="poorly defined editorial heirerchy" ID="ID_1061163319" CREATED="1381944526675" MODIFIED="1381944538439"/>
+<node TEXT="poor english" ID="ID_456814103" CREATED="1381944539803" MODIFIED="1381944545351"/>
+</node>
+</node>
+</node>
+<node TEXT="problem" ID="ID_614864658" CREATED="1381944729045" MODIFIED="1381944730369">
+<node TEXT="academic publishing powerhouses are sitting ath the top of the food chain" ID="ID_1719502506" CREATED="1381944730775" MODIFIED="1381944751211"/>
+</node>
+</node>
+</node>
+</node>
+</map>
